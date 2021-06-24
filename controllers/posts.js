674289210422d71ba980e7f0b2df9e436a09d951
shrishtiraghav <shrishti.raghav@eisenvault.com>
2021-06-24 14:17:21 +0530

    $inc operator used for increment

--- controllers/posts.js
+++ controllers/posts.js
@@ -58,7 +58,7 @@ export const likePost = async (req, res) => {
   if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
   const post= await PostMessage.findById(id);
-  const updatedPost = await PostMessage.findByIdAndUpdate(id ,{ likeCount: post.likeCount+1},{new: true});
+  const updatedPost = await PostMessage.findByIdAndUpdate(id ,{ $inc : {likeCount: 1}},{new: true});
 
   res.json(updatedPost);
 }
