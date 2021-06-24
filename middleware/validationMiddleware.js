d24a57046f834fdef26a9d20cd327fd9b2c0555d
shrishtiraghav <shrishti.raghav@eisenvault.com>
2021-06-24 16:27:01 +0530

    validation rule updated

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,24 +1,39 @@
-import { validator } from '../helpers/validate.js';
+import Validator from "validatorjs";
 
 export const createPostValidation = (req, res, next) => {
     const validationRule = {
-        "title": "required|string",
-        "message": "required|string",
-        "creator": "required|string",
-        "tags": "required|string",
-        "selectedFile": "required|string",
-    }
-    validator(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-            res.status(412)
-                .send({
-                    success: false,
-                    message: 'Validation failed',
-                    data: err
-                });
-        } else {
-            console.log("pass control to next function");
-            next();
-        }
-    });
-}
+                "title": "required|string",
+                "message": "required|string",
+                "creator": "required|string",
+                "tags": "required|string",
+                "selectedFile": "required|string",
+            }
+    const validation = new Validator(req.body, validationRule, customMessages);
+    validation.passes(() => {console.log("test passed"); next();});
+    validation.fails(() => callback(validation.errors, false));
+};
+
+// import { validator } from '../helpers/validate.js';
+
+// export const createPostValidation = (req, res, next) => {
+//     const validationRule = {
+//         "title": "required|string",
+//         "message": "required|string",
+//         "creator": "required|string",
+//         "tags": "required|string",
+//         "selectedFile": "required|string",
+//     }
+//     Validator(req.body, validationRule, {}, (err, status) => {
+//         if (!status) {
+//             res.status(412)
+//                 .send({
+//                     success: false,
+//                     message: 'Validation failed',
+//                     data: err
+//                 });
+//         } else {
+//             console.log("pass control to next function");
+//             next();
+//         }
+//     });
+// }
