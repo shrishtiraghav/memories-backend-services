4208c4f0a4dfc42fb1db7592a87311415ac0013c
shrishtiraghav <shrishti.raghav@eisenvault.com>
2021-06-24 15:23:56 +0530

    validation added for createPost

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,23 @@
+import validator from '../helpers/validate';
+
+export const createPostValidation = (req, res, next) => {
+    const validationRule = {
+        "title": "required|string",
+        "message": "required|string",
+        "creator": "required|string",
+        "tags": "required|string",
+        "selectedFile": "required|string",
+    }
+    validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+            res.status(412)
+                .send({
+                    success: false,
+                    message: 'Validation failed',
+                    data: err
+                });
+        } else {
+            next();
+        }
+    });
+}
