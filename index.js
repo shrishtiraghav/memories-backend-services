b072c6a7f854200e52794e0a5343f9384c407119
shrishtiraghav <shrishti.raghav@eisenvault.com>
2021-06-24 13:49:10 +0530

    PORT and ConnectionUrl

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from DeviceMotionEvent;
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,12 +19,10 @@ app.get('/' , (req,res) => {
   res.send('Hello to memories API');
 });
 
-const CONNECTION_URL =
-  "mongodb+srv://shrishtiraghav:shrishti123@cluster0.0d9ma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log("server running")))
   .catch((error) => console.log(error.message));
 
